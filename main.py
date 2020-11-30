4f3ec084510036cc56f50a1cb136869f55a10aa4
katiegirlsarah <69067202+katiegirlsarah@users.noreply.github.com>
2020-11-30 18:27:11 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import make_bambitext
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
 
 print("Hi")
 
